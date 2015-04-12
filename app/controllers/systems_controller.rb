f94d08088614fd059b6993a8fecffe05794590b5
Robert Martin <robert.martin@nbcuni.com>
2015-04-11 22:06:33 -0700

    use latest gem, and use time zone of system for display.

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -1,16 +1,24 @@
 class SystemsController < ApplicationController
+  around_filter :set_time_zone
 
   def home
-    @system = Enlighten::System.new(params[:id])
-    @summary = @system.summary
-    @inventory = @system.inventory
-    @envoys = @system.envoys
     @production = @system.monthly_production(:start_date=>1.month.ago - (3600*24))
-    @start_day
-    @stats = @system.stats(:start_at=>DateTime.new(Time.now.year,Time.now.month,Time.now.day,7,0), :end_at=>Time.now)
+    start_time = Time.now
+    @stats = @system.stats(:start_at=>Time.local(start_time.year,start_time.month,start_time.day,0,0), :end_at=>Time.now)
   end
 
   def graph
-    @intervals = Enlighten::System.new(params[:id]).stats(:start_at=>DateTime.new(Time.now.year,Time.now.month,Time.now.day,7,0), :end_at=>Time.now).intervals
+    start_time = Time.now
+    @intervals = @system.stats(:start_at=>Time.local(start_time.year,start_time.month,start_time.day,0,0), :end_at=>Time.now).intervals
   end
+
+  private
+    def set_time_zone
+      old_time_zone = Time.zone
+      @system = Enlighten::System.new(id: params[:id].to_i)
+      Time.zone = @system.timezone
+      yield
+    ensure
+      Time.zone = old_time_zone
+    end
 end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module EnlightenExample
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.default_time_zone ="Pacific Time (US & Canada)"
   end
 end
